b6fee0c7a11a968dc5ed43a178c0e16a912ba6a0
tomisaurio09 <tomisaurio09@gmail.com>
2025-12-06 20:05:32 -0300

    I addded the official URL for my backend into my middleware function

--- app/utils/middleware.py
+++ app/utils/middleware.py
@@ -1,7 +1,7 @@
 from flask import request
 from werkzeug.exceptions import Forbidden
 
-ALLOWED_HOSTS = {"localhost", "127.0.0.1", "api.studytrack.com"}
+ALLOWED_HOSTS = {"localhost", "127.0.0.1", "api.studytrack.com","studytrack-90ww.onrender.com"}
 
 def enforce_allowed_hosts(app):
     @app.before_request
